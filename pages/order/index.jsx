7563720f1cce796bdda176353eac6e13e499734b
codingwebsa <codingwebsa@gmail.com>
2023-01-05 07:13:08 -0800

    index page order redirect to recent order page

--- pages/order/index.jsx
+++ pages/order/index.jsx
@@ -1,5 +1,16 @@
+import { useRouter } from "next/router";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  const router = useRouter();
+  if (typeof window !== "undefined") {
+    const recentOrderID = localStorage.getItem("recentOrderID");
+    if (recentOrderID)
+      router.push("https://alhedaya.vercel.app/order/" + recentOrderID);
+
+    return;
+  }
+
+  return <div>Place a order first</div>;
 };
 
 export default Orders;
